6f0ee7ccf6fefaaf412151127fe0b9b6d483b000
Your Name <you@example.com>
2020-07-19 17:37:29 +0700

    bed-motor v2.0 (control-from-http)

--- api.py
+++ api.py
@@ -1,91 +1,120 @@
 from flask import Flask, json, request
-import libs.bed_motor as BedMotor
-
-# import sys
-
-# # init-Flask:
-# sudo apt-get install python3-pip python3-dev 
-# sudo apt-get install python3-venv
-
-# python3 -m venv env
-
-# source env/bin/activate
-
-# pip install uwsgi flask
-
-# #check python-version
-# python --version
-# pip freeze
-
-'''
-v1(control-from-app)
-| api.py -> bed_motor.py(extend-M(HatMdd10)]) 
-                    -> drive_linear_motor.py(M[1,2][A,B])
-
-Exception-list:
-    - show //response and show in APP(Color=Yellow)
-    - Bug //response and show in APP(Color=Red)
-'''
+from threading import Thread
+import libs.drive_linear_motor as BedMotor
 
 
-# NOTE: path-API
-#    # /debug/ [1a,1b,10,  2a,2b,20, f167, f2[ความถี่ Hz] 
-
-HatMdd10 = BedMotor.HatMdd10
-
-api = Flask(__name__)
 # Flask-Env:
 # # on windows: set FLASK_APP=api.py
-# # on Linux: export FLASK_APP=api.py
+'''
+on Linux: export FLASK_APP=api.py
+on Linux: export FLASK_ENV=development
 
-# # on Linux: export FLASK_ENV=development
-# # on Linux: export FLASK_APP=bedmotor.py
+python3 api.py
+'''
 
-# Debug Mode:
-# # export FLASK_ENV=development
 # run:
 # # flask run
 # # flask run --host=0.0.0.0
 
-# python--import
-# https://docs.python.org/3/tutorial/modules.html#intra-package-references
-# from . import echo
-# from .. import formats
-# from ..filters import equalizer
+# NOTE: flask-API
+#    # /stop //for Stop Server (ฉุกเฉิน)
+#    # /debug/ [1a=rf,1b=rb,10=r0], [2a=lf,2b=lb,20=l0]
 
 
-# /debug/1a
-@api.route('/debug/<mode>', methods=['GET'])
-def get_debug(mode):
-    print("GET: /debug/"+mode)
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
-    # motor-1
-    if mode == "1a":
-        # attrs = vars(HatMdd10)
-        # print(attrs)
-        HatMdd10.m1AB()
-    elif mode == "1b":
-        HatMdd10.m1BA()
-    elif mode == "10":
-        HatMdd10.m1Is0()
 
-    # motor-2
-    elif mode == "2a":
-        HatMdd10.m2AB()
-    elif mode == "2b":
-        HatMdd10.m2BA()
-    elif mode == "20":
-        HatMdd10.m2Is0()
-        
-    elif mode[0] == "f":
-        if mode[1] == "1":
-            HatMdd10.changeFrequency1(int(mode[2:]))
-        elif mode[1] == "2":
-            HatMdd10.changeFrequency2(int(mode[2:]))
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
-    return json.dumps({"dataStatus": "success"})
+
+def create_app(HatMdd10):
+    # https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/
+
+    app = Flask(__name__)
+
+    # Enable Cors for web-browser test
+    @app.after_request
+    def after_request(response):
+        header = response.headers
+        header['Access-Control-Allow-Origin'] = '*'
+        return response
+
+    # set response mimetype=JSON
+    def myRes(data, headerStatus=200):
+        response = app.response_class(
+            response=json.dumps(data),
+            status=headerStatus,
+            mimetype='application/json'
+        )
+        return response
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        print("GET: /index")
+        return 'Hello, World!'
+
+    # http://127.0.0.1:5000/stop
+    @app.route('/stop', methods=['GET'])
+    def stop():
+        print("GET: /stop")
+        print("Shutdown: with Hard-STOP API(/stop")
+        shutdown()
+        return myRes({"dataStatus": "success", 'data': 'stoped'})
+
+    # /debug/ []
+    @app.route('/debug/<command>', methods=['GET'])
+    def get_debug(command):
+        print("GET: /debug/"+command)
+
+        # motor-1
+        if command == "1a":
+            HatMdd10.m1AB()
+            message = 'rf'
+        elif command == "1b":
+            HatMdd10.m1BA()
+            message = 'rb'
+        elif command == "10":
+            HatMdd10.m1Is0()
+            message = 'r0'
+
+        # motor-2
+        elif command == "2a":
+            HatMdd10.m2AB()
+            message = 'lf'
+        elif command == "2b":
+            HatMdd10.m2BA()
+            message = 'lb'
+        elif command == "20":
+            HatMdd10.m2Is0()
+            message = 'l0'
+
+        else:
+            return myRes({"dataStatus": "error", 'message': 'not command: ' + command}, 404)
+
+        return myRes({"dataStatus": "success", "message": message})
+
+    return app
+
+
+def main():
+    HatMdd10 = BedMotor.HatMdd10()
+    try:
+        app = create_app(HatMdd10)
+        app.run(host='0.0.0.0')
+    except Exception as err:
+        print(str(err.args))
+    except:
+        print('some Err: server is Stoped.')
+
+    HatMdd10.cleanup()
 
 
 if __name__ == '__main__':
-    # api.run()
-    api.run(host='0.0.0.0')
+    main()
--- libs/drive_linear_motor.py
+++ libs/drive_linear_motor.py
@@ -1,7 +1,15 @@
 import atexit
 import os
 
+import time
+# import sqlite3
 import RPi.GPIO as GPIO
+
+
+# set ENV:
+PWM_DUTY_CYCLE = 100
+PWM_FREQUENCY = 100
+PWM_TIME_WAIT = 0.07
 
 
 class HatMdd10:
@@ -18,75 +26,149 @@ class HatMdd10:
 
     '''
 
-    def __init__(self, f):
+    # def connDB(self):
+    #     DATABASE = 'database.sqlite'
+    #     conn = sqlite3.connect(DATABASE)
+    #     conn.row_factory = lambda c, r: dict(
+    #         zip([col[0] for col in c.description], r))
+
+    #     return conn
+
+    # def isRunning(self, c):
+    #     c.execute("SELECT * FROM settings WHERE key=?", ('isRunning',))
+    #     row = c.fetchone()
+    #     return row['value']
+
+    # def cancleRunning(self, conn):
+    #     c = conn.cursor()
+    #     c.execute('UPDATE settings SET value = ? WHERE key = ?',
+    #               (1, 'isRunning'))
+    #     conn.commit()
+
+    # def waitStopOldProcess(self, conn):
+    #     c = conn.cursor()
+    #     print('waitStopOldProcess()')
+    #     for _ in range(15):
+    #         if(self. isRunning(c) == int(False)):
+    #             break
+    #         self.cancleRunning(conn)
+    #         time.sleep(1.1)
+
+    #     print('END-waitStopOldProcess()')
+
+    def __init__(self, f=PWM_FREQUENCY, dutyCycle=PWM_DUTY_CYCLE):
+        # DEBUG-only
+        self.isCleaned = 0
+
         print("__init__")
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        # conn = self.connDB()
+
+        # raise Exception("Sorry, no numbers below zero", 2)
+        # self.waitStopOldProcess(conn)
+        # init-dutyCycle
+        # self.dutyCycle = 26
+        self.dutyCycle = dutyCycle
+        # init-frequency
+        self.frequency = f
+        # init-waitHardware
+        self.waitHardwareLong = PWM_TIME_WAIT
+        # init-pin
         self.__pin_dir1 = 26
         self.__pin_dir2 = 24
         self.__pin_pwm1 = 12
         self.__pin_pwm2 = 13
-        # init-pin
+        # setup-GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        # setup-pin
         GPIO.setup(self.__pin_dir1, GPIO.OUT)		# set pin as output
         GPIO.setup(self.__pin_dir2, GPIO.OUT)		# set pin as output
         GPIO.setup(self.__pin_pwm1, GPIO.OUT)		# set pin as output
         GPIO.setup(self.__pin_pwm2, GPIO.OUT)		# set pin as output
         # init-PWM
-        self.m1 = GPIO.PWM(self.__pin_pwm1, f)			# set pwm for M1
-        self.m2 = GPIO.PWM(self.__pin_pwm2, f)			# set pwm for M2
-        self.m1.start(0)
-        self.m2.start(0)
+        self.m1 = GPIO.PWM(self.__pin_pwm1, self.frequency)			# set pwm for M1
+        self.m2 = GPIO.PWM(self.__pin_pwm2, self.frequency)			# set pwm for M2
+        # clear-oldState-GPIO
+        self.clearPwm()
+        self.clearDir()
+        # self.m1.stop()
+        # self.m2.stop()
+        # self.m1.start(0)
+        # self.m2.start(0)
         # Listen-Cleanup()
         atexit.register(self.cleanup)
 
-    def cleanup(self):
-        print("Running cleanup...")
-        self.clearAll()
+    # def __enter__(self):
+    #     GPIO.setmode(GPIO.BCM)
+    #     GPIO.setwarnings(False)
+    #     # setup-pin
+    #     GPIO.setup(self.__pin_dir1, GPIO.OUT)		# set pin as output
+    #     GPIO.setup(self.__pin_dir2, GPIO.OUT)		# set pin as output
+    #     GPIO.setup(self.__pin_pwm1, GPIO.OUT)		# set pin as output
+    #     GPIO.setup(self.__pin_pwm2, GPIO.OUT)		# set pin as output
+    #     # init-PWM
+    #     self.m1 = GPIO.PWM(self.__pin_pwm1, self.frequency)			# set pwm for M1
+    #     self.m2 = GPIO.PWM(self.__pin_pwm2, self.frequency)			# set pwm for M2
+    #     self.m1.stop()
+    #     self.m2.stop()
+    #     return self
+
+    # wait Hardware
+    def waitHardware(self):
+        time.sleep(self.waitHardwareLong)
 
     # motor-All zero
     def mZero(self):
+        self.waitHardware()
         self.m1Is0()
         self.m2Is0()
 
     # motor-1 function
     def m1Is0(self):
-        self.m1.ChangeDutyCycle(0)
+        self.waitHardware()
+        # self.m1.stop()
+        self.m1.start(0)
         GPIO.output(self.__pin_dir1, GPIO.LOW)
 
     def m1AB(self):
-        self.m1.ChangeDutyCycle(25)
+        self.waitHardware()
+        self.m1.start(self.dutyCycle)
         GPIO.output(self.__pin_dir1, GPIO.LOW)
 
     def m1BA(self):
-        self.m1.ChangeDutyCycle(25)
+        self.waitHardware()
+        self.m1.start(self.dutyCycle)
         GPIO.output(self.__pin_dir1, GPIO.HIGH)
 
     # motor-2 function
     def m2Is0(self):
-        self.m2.ChangeDutyCycle(0)
+        self.waitHardware()
+        # self.m2.stop()
+        self.m2.start(0)
         GPIO.output(self.__pin_dir2, GPIO.LOW)
 
     def m2AB(self):
-        self.m2.ChangeDutyCycle(25)
+        self.waitHardware()
+        self.m2.start(self.dutyCycle)
         GPIO.output(self.__pin_dir2, GPIO.LOW)
 
     def m2BA(self):
-        self.m2.ChangeDutyCycle(25)
+        self.waitHardware()
+        self.m2.start(self.dutyCycle)
         GPIO.output(self.__pin_dir2, GPIO.HIGH)
 
-    # changeFrequency[1,2]
-    def changeFrequency1(self, newFrequency):
-        self.m1.ChangeFrequency(newFrequency)
-
-    def changeFrequency2(self, newFrequency):
-        self.m2.ChangeFrequency(newFrequency)
-
     # clearGPIO
+
+    # def __exit__(self, exc_type, exc_value, traceback):
+    #     self.cleanup()
+
     def clearPwm(self):
+        self.waitHardware()
         self.m1.stop()
         self.m2.stop()
 
     def clearDir(self):
+        self.waitHardware()
         GPIO.output(self.__pin_dir1, GPIO.LOW)
         GPIO.output(self.__pin_dir2, GPIO.LOW)
 
@@ -95,15 +177,42 @@ class HatMdd10:
         self.clearDir()
         GPIO.cleanup()  # this ensures a clean exit
 
+    def cleanup(self):
+        print("Running cleanup...")
+        try:
+            self.clearAll()
+            self.testErr()
+        except Exception as err:
+            print("!!!!HatMdd10::cleanup() more one times..")
+            print(str(err.args))
+        except:
+            print("!!!!HatMdd10::cleanup() more one times..")
+            print("Other error when HatMdd10::cleanup()!")
+        print("HatMdd10::cleanup()")
+
+    # DEBUG-only
+    def testErr(self):
+        raise AttributeError('HatMdd10::testErr()')
+
 
 def main():
-    print("Drive-Linear-Motor:")
+    print("DEBUG-Linear-Motor:")
 
-    motor = HatMdd10(67)
+    # HatMdd10(67)
+    # motor = HatMdd10(2)
+    motor = HatMdd10(PWM_FREQUENCY, PWM_DUTY_CYCLE)
+    # with HatMdd10(PWM_FREQUENCY, PWM_DUTY_CYCLE) as motor:
+    #     pass
+    # cleanup
+    # https://raspi.tv/2013/rpi-gpio-basics-3-how-to-exit-gpio-programs-cleanly-avoid-warnings-and-protect-your-pi
     try:
         while True:
             x = input("enter input:")
             print("input: " + x)
+            # testErr
+            if x == "err":
+                motor.testErr()
+
             # motor-1
             if x == "1a":
                 motor.m1AB()
@@ -122,19 +231,25 @@ def main():
 
             elif x[0] == "f":
                 if x[1] == "1":
-                    motor.changeFrequency1(int(x[2:]))
+                    motor.cleanup()
+                    # motor.changeFrequency1(int(x[2:]))
                 elif x[1] == "2":
-                    motor.changeFrequency2(int(x[2:]))
+                    pass
+                    # motor.changeFrequency2(int(x[2:]))
 
     except KeyboardInterrupt:
-        print("\n", "CTRL+C")
+        # here you put any code you want to run before the program
+        # exits when you press CTRL+C
+        print("\n", "CTRL+C")  # print value of counter
+
+    except Exception as err:
+        print(str(err.args))
 
     except:
-        print("Other error or exception occurred!")
+        print("Other error[in Main()] or exception occurred!")
 
-    finally:
-        # motor.clearAll()
-        print("exit()")
+    motor.cleanup()
+    print("exit()")
 
 
 if __name__ == "__main__":
